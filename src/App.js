e48bd78f76f525eb22eb8a990be335a0ed388e3e
Chin Kuay <kuaychin@gmail.com>
2020-04-05 09:29:24 -0500

    modified App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to React</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
